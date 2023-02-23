84557f2ba03539715af6f52e43d0e7aa70f1ea60
phf1001 <phf1001@alu.ubu.es>
2023-02-23 19:50:03 +0100

    [S15] src: css cutre

--- src/web/flaskr/reports.py
+++ src/web/flaskr/reports.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for, session
 from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
@@ -7,4 +7,6 @@ bp = Blueprint('reports', __name__)
 
 @bp.route('/general_report', methods=['GET'])
 def general_report():
-    return render_template('reports/general_report.html')
+    messages = session['messages']
+    url = messages.get('url')
+    return render_template('reports/general_report.html', url=url)
--- src/web/flaskr/scanner.py
+++ src/web/flaskr/scanner.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for, session
 from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
+import json
 
 bp = Blueprint('scanner', __name__)
 
@@ -9,7 +10,7 @@ bp = Blueprint('scanner', __name__)
 def index():
     
     if request.method == 'POST':
-        print(request.form["url"])
+        session['messages'] = { 'url' : request.form['url'] }
         return redirect(url_for('reports.general_report'))
 
     return render_template('scanner/index.html')
