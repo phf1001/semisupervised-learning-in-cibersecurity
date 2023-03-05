20034a1545a1c93f219b9295af164a4a614d7e09
Patricia Hernando <phf1001@alu.ubu.es>
2023-03-05 10:31:20 +0100

    [S15] src: postgresql heroku connection x2

--- src/krini-frontend/apps/config.py
+++ src/krini-frontend/apps/config.py
@@ -16,8 +16,8 @@ class Config(object):
 
     database_url = os.environ.get('DATABASE_URL')
 
-    if database_url.startswith('postgres://'):
-        database_url.replace('postgres://', 'postgresql://')
+    if database_url and database_url.startswith('postgres://'):
+        database_url = database_url.replace('postgres://', 'postgresql://', 1)
 
     SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
