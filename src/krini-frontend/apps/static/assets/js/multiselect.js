0aa797f6d0aea6b8c2134d700f885dd576a1ab11
Patricia Hernando <phf1001@alu.ubu.es>
2023-03-14 13:14:27 +0100

    [S17] web: multiselector index integration. NO functionality.

--- src/krini-frontend/apps/static/assets/js/multiselect.js
+++ src/krini-frontend/apps/static/assets/js/multiselect.js
@@ -3,7 +3,6 @@ items = document.querySelectorAll('.item');
 
 selectBtn.addEventListener('click', () => {
     selectBtn.classList.toggle('open');
-
 });
 
 items.forEach(item => {
@@ -11,12 +10,24 @@ items.forEach(item => {
         item.classList.toggle("checked");
 
         let checked = document.querySelectorAll('.checked');
+        let checked_names = document.querySelectorAll('.checked').forEach(item => item.innerText);
+
         btnText = document.querySelector('.btn-text');
         
         if (checked && checked.length > 0) {
             btnText.innerText = `Selected (${checked.length})`;
+            
         } else {
             btnText.innerText = "Select your models";
         }
+
+        let selected_names = [];
+
+        for(var i=0; i<checked.length; i++){
+            selected_names.push(checked[i].innerText);
+        }
+
+        console.log(selected_names)
+
     })
 })
