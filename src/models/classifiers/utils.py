7830984d0fb41b78298e77b1c70e10de39c315c6
Patricia Hernando <phf1001@alu.ubu.es>
2023-04-28 11:52:02 +0200

    [S20] deploy: removed statsmodels, Heroku failing

--- src/models/classifiers/utils.py
+++ src/models/classifiers/utils.py
@@ -7,27 +7,42 @@
 @Version :   1.0
 @Contact :   phf1001@alu.ubu.es
 """
-
-from statsmodels.stats.proportion import proportion_confint
+# Statmodels commented since it's giving problems with Heroku
+# from statsmodels.stats.proportion import proportion_confint
 from math import sqrt
 from scipy import stats
 
 
 def confidence_interval(cls, L, y):
     """
-    Returns the confidence interval
-    for the classifier cls given a set
+    Returns the confidence interval  for the classifier cls given a set
     of labeled data L and the labels y.
+
+    Args:
+        cls (object): classifier
+        L (np.array): labeled data
+        y (np.array): labels
+
+    Returns:
+        tuple: confidence interval
     """
-    return self_confidence_interval_method_3(cls, L, y)
+    return confidence_interval_method_1(cls, L, y)
 
 
 def confidence_interval_method_1(cls, L, y):
     """
-    Returns the confidence interval
-    for the classifier cls given a set
+    Returns the confidence interval  for the classifier cls given a set
     of labeled data L and the labels y.
-    Suggested by César Ignacio García.
+
+    Suggested by César Ignacio García Osorio.
+
+    Args:
+        cls (object): classifier
+        L (np.array): labeled data
+        y (np.array): labels
+
+    Returns:
+        tuple: confidence interval
     """
     cte = stats.norm.isf(0.05 / 2.0)
     y_pred = cls.predict(L)
@@ -41,10 +56,18 @@ def confidence_interval_method_1(cls, L, y):
 
 def confidence_interval_method_2(cls, L, y):
     """
-    Returns the confidence interval
-    for the classifier cls given a set
+    Returns the confidence interval  for the classifier cls given a set
     of labeled data L and the labels y.
-    Suggested by Álvar Arnaiz.
+
+    Suggested by Álvar Arnaiz González.
+
+    Args:
+        cls (object): classifier
+        L (np.array): labeled data
+        y (np.array): labels
+
+    Returns:
+        tuple: confidence interval
     """
     y_pred = cls.predict(L)
     n_total = len(y)
@@ -63,16 +86,27 @@ def confidence_interval_method_2(cls, L, y):
     return ((left - sq) / div, (left + sq) / div)
 
 
-def self_confidence_interval_method_3(cls, L, y):
-    """
-    Returns the confidence interval
-    for the classifier cls given a set
-    of labeled data L and the labels y.
-    Suggested by Jose Luis Garrido.
-    """
-    y_pred = cls.predict(L)
-    n_total = len(y)
-    n_hits = (y_pred == y).sum()
+# def confidence_interval_method_3(cls, L, y):
+#     """
+#     Returns the confidence interval  for the classifier cls given a set
+#     of labeled data L and the labels y.
 
-    li, hi = proportion_confint(n_hits, n_total, alpha=0.05, method="wilson")
-    return li, hi
+#     Commented because it's giving problems with Heroku.
+
+#     Suggested by José Luis Garrido Labrador.
+
+#     Args:
+#         cls (object): classifier
+#         L (np.array): labeled data
+#         y (np.array): labels
+
+#     Returns:
+#         tuple: confidence interval
+#     """
+#     y_pred = cls.predict(L)
+#     n_total = len(y)
+#     n_hits = (y_pred == y).sum()
+
+#     li, hi = proportion_confint(n_hits, n_total, alpha=0.05, method="wilson")
+#     return li, hi
+#     return 0, 0
