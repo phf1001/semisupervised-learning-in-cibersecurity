688b6898256c4c5b2c3fb4ef4f60c6371bbb3b31
Patricia Hernando <phf1001@alu.ubu.es>
2023-05-02 08:41:04 +0200

    [S20] src: dataset ordered

--- src/scripts/alexa_order.py
+++ src/scripts/alexa_order.py
@@ -4,7 +4,7 @@
 @File    :   alexa_order.py
 @Time    :   2023/03/30 20:58:27
 @Author  :   Patricia Hernando Fernández 
-@Version :   1.0
+@Version :   2.0
 @Contact :   phf1001@alu.ubu.es
 """
 
@@ -12,9 +12,11 @@ from difflib import SequenceMatcher
 import os
 import sys
 import csv
+import pandas as pd
 
 src_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 sys.path.append(src_path)
+
 from phishing_fvg.phishing_utils import (
     get_alexa_sites,
     get_data_path,
@@ -22,21 +24,24 @@ from phishing_fvg.phishing_utils import (
 )
 
 ordered_list = get_csv_data(get_data_path() + os.sep + "alexa_top_10k.csv")
-set_alexa = get_alexa_sites()
+# set_alexa = get_alexa_sites()
+set_alexa = pd.read_csv("genuine_pd.csv", delimiter=";")
+
 ordered_working = []
 
 for element in ordered_list:
     max_similarity = 0
     item = None
 
-    for element_2 in set_alexa:
+    for index, row in set_alexa.iterrows():
+        element_2 = row["url"]
         new_similarity = SequenceMatcher(None, element, element_2).ratio()
 
         if new_similarity > max_similarity:
-            item = element_2
+            item = row
             max_similarity = new_similarity
 
-    ordered_working.append([item])
+    ordered_working.append([item["url"], item["fv"], item["tag"]])
 
     if len(ordered_working) == len(set_alexa):
         break
@@ -45,7 +50,7 @@ for element in ordered_list:
 output_file = get_data_path() + os.path.sep + "alexa_ordered_working_urls.csv"
 with open(output_file, mode="w") as f:
     writer = csv.writer(
-        f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        f, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL
     )
 
     for url in ordered_working:
